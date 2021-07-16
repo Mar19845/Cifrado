0ad25d36afed7ee81589c6b7950cdc4ac4341f88
Mar19845 <mar19845@uvg.edu.gt>
2021-07-15 20:30:09 -0600

    Update main.py

--- Lab1/main.py
+++ Lab1/main.py
@@ -19,5 +19,31 @@ def cleanTxt(tx):
         t = t.replace(w, '')
     return t
 
-t1 = cleanTxt(texto)
+def ceasar(text,desp):
+    text=cleanTxt(text)
+    cipher = ''
+    for i in range(len(text)):
+        char = text[i]
+        if (char.islower()):
+            cipher += chr((ord(char) + desp - 97) % 26 + 97)  # 97 letra minuscula (a)
+        else:
+            cipher += chr((ord(char) + desp-65) % 26 + 65)  # Prima letra mayuscula (A)
+    return cipher
+
+def caesar_d(text,desp):
+    text=cleanTxt(text)
+    plain = ""
+    for i in range(len(text)):
+        char = text[i]
+        if (char.islower()):
+            plain += chr((ord(char) -97-desp) % 26 + 97)
+        else:
+            plain += chr((ord(char) -65-desp) % 26 + 65)
+    return plain
+
+
+t1 = ceasar(texto,4)
 print(t1)
+
+t2 = caesar_d(t1,4)
+print(t2)
