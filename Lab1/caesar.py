4905280b28c04af7aafcd95aec0457d09ca12cc6
eerh <eduardorh1312@gmail.com>
2021-07-17 17:31:24 -0600

    cambios en caesar porque eran 27 no 26

--- Lab1/caesar.py
+++ Lab1/caesar.py
@@ -7,9 +7,9 @@ def encryptC(mensaje, desp):
     for i in range(len(mensaje)):
         char = text[i]
         if (char.islower()):
-            cipher += chr((ord(char) + desp - 97) % 26 + 97)  # 97 letra minuscula (a)
+            cipher += chr((ord(char) + desp - 97) % 27 + 97)  # 97 letra minuscula (a)
         else:
-            cipher += chr((ord(char) + desp - 65) % 26 + 65)  # Prima letra mayuscula (A)
+            cipher += chr((ord(char) + desp - 65) % 27 + 65)  # Prima letra mayuscula (A)
     
     return cipher
 
@@ -20,8 +20,8 @@ def decryptC(mensaje, desp):
     for i in range(len(mensaje)):
         char = text[i]
         if (char.islower()):
-            plain += chr((ord(char) -97-desp) % 26 + 97)
+            plain += chr((ord(char) -97-desp) % 27 + 97)
         else:
-            plain += chr((ord(char) -65-desp) % 26 + 65)
+            plain += chr((ord(char) -65-desp) % 27 + 65)
     
     return plain
