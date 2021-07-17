5f1951085a26506ee5f571fab033003c033b36d2
Carlos21122000 <craxtum1@gmail.com>
2021-07-16 22:03:38 -0600

    carga

--- Lab1/vigenere.py
+++ Lab1/vigenere.py
@@ -1,7 +1,16 @@
-def decryptV():
-    
-    print("DESENCRIPTADO\n")
+def vigenere_decipher(text,clave):
+    result = ""
+    for i in range(0,len(text)): #Para todo el texto
+        desp = get_desp(clave[i % len(clave)]) #Obtenemos el desplazamiento asociado a la clave
+        result +=(caesar_decipher(text[i],desp)) #desciframos la letra con caesar para el desplazamiento dado
+    print("Decifrado")
+    return result
 
-def encryptV():
-    
-    print("ENCRIPTADO\n")
+
+def vigenere_cipher(plain,clave):
+    result = ""
+    for i in range(0,len(plain)): #Para todo el texto
+       desp = get_desp(clave[i % len(clave)]) #Desplazamiento asociado a la letra clave
+       result +=(caesar_cipher(plain[i],desp)) #Ciframos la letra con caesar
+       print("Cifrado")
+    return result
