2db5d5b327e39b0a5ee13030aa09ac3099f8d668
Mar19845 <mar19845@uvg.edu.gt>
2021-07-19 08:30:54 -0600

    cambios en main y function

--- Lab1/functionality.py
+++ Lab1/functionality.py
@@ -77,3 +77,18 @@ def vigenereInit():
     print("")
     #decryptV()
     
+def distribucion():
+    mensaje = input("Ingrese un texto Cifrado o descrifrado: ")
+    mensaje = cleanTxt(mensaje)
+    dist = {
+        'A':0,'B':0,'C':0,'D':0,'E':0,'F':0,'G':0,'H':0,'I':0,'J':0,'K':0,'L':0,'M':0,'N':0,'Ñ':0,'O':0,'P':0,'Q':0,'R':0,'S':0,'T':0,'U':0,'V':0,'W':0,'X':0,'Y':0,'Z':0
+    }
+    for i in mensaje:
+        for j in alpha:
+            if i==j:
+                dist[i]=dist.get(i) + 1
+    
+    for i in dist:
+        dist[i]= float(dist.get(i)/len(mensaje))
+
+    return dist
--- Lab1/main.py
+++ Lab1/main.py
@@ -11,7 +11,7 @@ st = True
 
 print("*****ANALISIS DE FUERZA BRUTA*****\n")
 while(st):
-    print(" 1.Caesar \n 2.Afín \n 3.Vigenère\n 4.SALIR\n")
+    print(" 1.Caesar \n 2.Afín \n 3.Vigenère\n 4.Distribuciones\n 5.SALIR\n")
     opt = int(input("Elija el metodo que desea utilizar: " ))
 
     if opt == 1:
@@ -28,13 +28,16 @@ while(st):
         
         print("\n*****VIGENERE METHOD*****\n")
         vigenereInit()
-
     if opt == 4:
+        
+        print("\n*****DISTRIBUCCION*****\n")
+        distribucion()
+    if opt == 5:
         
         print("\n*****FIN DEL PROGRAMA*****\n")
         st = False
 
-    if opt<1 or opt>4:
+    if opt<1 or opt>5:
         print("\nOpción no valida, intente de nuevo\n")
         
     
