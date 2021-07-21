473cdef8c0e21a68f9be18b7d2dc81ee869d88f0
Carlos21122000 <craxtum1@gmail.com>
2021-07-21 17:23:19 -0600

    bruto cesar

--- Lab1/main.py
+++ Lab1/main.py
@@ -11,7 +11,7 @@ st = True
 
 print("*****ANALISIS DE FUERZA BRUTA*****\n")
 while(st):
-    print(" 1.Caesar \n 2.Afín \n 3.Vigenère\n 4.Distribuciones\n 5.SALIR\n")
+    print(" 1.Caesar \n 2.Afín \n 3.Vigenère\n 4.Distribuciones\n 5.Fuerza Bruta\n 6.SALIR\n")
     opt = int(input("Elija el metodo que desea utilizar: " ))
 
     if opt == 1:
@@ -33,11 +33,29 @@ while(st):
         print("\n*****DISTRIBUCCION*****\n")
         distribucion()
     if opt == 5:
+        while(st):
+            print(" 1.Caesar \n 2.Afín \n 3.Vigenère\n 4.SALIR\n")
+            opt = int(input("Elija el metodo que desea utilizar: " ))
+
+            if opt == 1:
+                print("\n*****CAESAR METHOD*****\n")
+                BrutoC()
+            if opt == 2:         
+                print("\n*****AFIN METHOD*****\n")
+                afinInit()
+            if opt == 3:   
+                print("\n*****VIGENERE METHOD*****\n")
+                vigenereInit()
+                    
+            if opt<1 or opt>3:
+                print("\nOpción no valida, intente de nuevo\n")
+                st = False
+                
+    if opt == 6:
         
         print("\n*****FIN DEL PROGRAMA*****\n")
         st = False
-
-    if opt<1 or opt>5:
+    if opt<1 or opt>6:
         print("\nOpción no valida, intente de nuevo\n")
         
     
