f6c5e43c042a86ab5124a2c418b04fc6159e4ab3
Mar19845 <mar19845@uvg.edu.gt>
2021-07-23 21:43:41 -0600

    creacion punto 2 del lab

---
+++ Lab2/Base64.py
@@ -0,0 +1,25 @@
+import base64
+  
+
+
+
+def stringToBase64(text):
+    sample_string_bytes = text.encode("ascii")
+    base64_bytes = base64.b64encode(sample_string_bytes)
+    base64_string = base64_bytes.decode("ascii")
+    return base64_string
+
+
+def Base64ToString(text):
+    base64_bytes = text.encode("ascii")
+    
+    sample_string_bytes = base64.b64decode(base64_bytes)
+    sample_string = sample_string_bytes.decode("ascii")
+    return sample_string
+
+s = input('Ingresa un texto para convertirlo a base64   ')
+d = stringToBase64(s)
+f = Base64ToString(d)
+print('\n')
+print(d)
+print(f)
