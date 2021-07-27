dd3363df080ca183c65adcbe35ef02a6583a3413
Carlos21122000 <craxtum1@gmail.com>
2021-07-27 15:58:20 -0600

    parte 3b

--- Lab2/xor.py
+++ Lab2/xor.py
@@ -1,4 +1,5 @@
 import cleaner as cl
+import matplotlib.pyplot as plt
 
 def toBits(c1, c2):
 
@@ -16,6 +17,18 @@ def toBits(c1, c2):
     result = list(map(lambda x, y: x ^ y, bits, bits1))
     result = cl.listJoiner(result, '')
     
+    result1 = []
+    result1 = list(map(lambda x, y: x ^ y, bits, bits1))
+    n, bins, patches=plt.hist(bits1)
+    plt.xlabel("Values")
+    plt.ylabel("Frequency")
+    plt.title("Histogram")
+    plt.show()
+
     return result
 
+   
+
+
+    
     
