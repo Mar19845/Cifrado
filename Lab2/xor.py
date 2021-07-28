f04ed03420fe83039780e1ac4126fbc08fd4f78c
Mar19845 <mar19845@uvg.edu.gt>
2021-07-28 03:24:43 -0600

    XOR mejorada

--- Lab2/xor.py
+++ Lab2/xor.py
@@ -1,5 +1,6 @@
 import cleaner as cl
 import matplotlib.pyplot as plt
+import random
 
 def xori(c1, c2):
 
@@ -27,8 +28,46 @@ def xori(c1, c2):
 
     return result
 
+def rand_Bits(p):
+    #codigo extraido de 
+    #https://www.geeksforgeeks.org/python-program-to-generate-random-binary-string/
+    # Variable to store the 
+    # string
+    key1 = ""
+  
+    # Loop to find the string
+    # of desired length
+    for i in range(p):
+          
+        # randint function to generate
+        # 0, 1 randomly and converting 
+        # the result into str
+        temp = str(random.randint(0, 1))
+  
+        # Concatenatin the random 0, 1
+        # to the final result
+        key1 += temp
+          
+    return key1
    
+def XOR(chain):
+    chain2 = rand_Bits(len(chain))
+    
+    bits = []
+    
+    for i in chain:
+        bits.append(i)
+    bits = list(map(int, bits))
+
+    bits1 = []
+    for j in chain2:
+        bits1.append(j)
+    bits1 = list(map(int, bits1))
+
+    result = list(map(lambda x, y: x ^ y, bits, bits1))
+    result = cl.listJoiner(result, '')
+    
 
 
-    
-    
+
+
