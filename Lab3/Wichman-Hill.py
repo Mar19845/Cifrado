03ba04df511d03366e0c12b249b3775dd60e6226
Carlos21122000 <craxtum1@gmail.com>
2021-08-12 16:18:34 -0600

    wichman-hill generator

---
+++ Lab3/Wichman-Hill.py
@@ -0,0 +1,18 @@
+import random
+from random import randint
+
+def Wichmann_Hill(val1, val2):
+    seed1 = randint(1, 30000)
+    seed2 = randint(1, 30000)
+    seed3 = randint(1, 30000)
+    
+    numlist = []
+    for i in range(listlength):
+       
+        seed1 = 171 * seed1 % 30269
+        seed2 = 172 * seed2 % 30307
+        seed3 = 170 * seed3 % 30323
+
+        numlist.append((float(seed1)/30269.0 + float(seed2)/30307.0 + float(seed3)/30323.0) % 1.0)
+    #print(numlist[0:50])
+    return numlist
