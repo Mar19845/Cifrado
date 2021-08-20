b8fc1f86b5449d18b3817658151e4f84057d3eee
Mar19845 <mar19845@uvg.edu.gt>
2021-08-19 23:55:13 -0600

    lcg new

--- Lab4/ejercicio3.py
+++ Lab4/ejercicio3.py
@@ -3,11 +3,12 @@ import lcg as lcg
 import lfsr as lfsr
 import WichmanHill as Wichman_Hill
 import matplotlib.pyplot as plt
+import math
+import numpy as np
 
 #https://github.com/GINARTeam/NIST-statistical-test/blob/master/01_monobit_test.py
-#monobit_test
-def test(input, n):
-        
+def test(input):
+    n = len(input)  
     ones = input.count('1') #number of ones
 
     zeroes = input.count('0')    #number of zeros
@@ -18,17 +19,28 @@ def test(input, n):
 
     success = ( p >= 0.01)  # success = true if p-value >= 0.01
 
-    return [zeroes, ones, s, p, success]
+    return success
 
 def histogroma(lista):
-    print('ads')
+    guds = 0
+    fail = 0
+    n = len(lista)
+    for i in lista:
+        x= test(i)
+        if x == True:
+            guds+=1
+        if x == False:
+            fail += 1
+    print(guds,fail)
+    plt.hist(x=[guds,fail])
+    plt.show()
 def init_histo(n):
     histo=[]
     for i in range(1000):
-        histo.append(lcg.init_lcg(n))
+        histo.append(lcg.LCG(n=n))
         #histo.append(lfsr.lfsr(n,[4, 2, 5], 6))
         #histo.append(Wichman_Hill.Wichmann_Hill(n))
 
     histogroma(histo)
 
-init_histo(10)
+init_histo((255))
--- Lab4/lcg.py
+++ Lab4/lcg.py
@@ -1,28 +1,16 @@
 import random
 from random import seed
 from random import randint
+import numpy as np
 
-def LCG( x,a, c, m):
-    while True:
-        x = (a * x + c) % m
-        yield x
-
-
-def init_lcg(n):
-    x = random.randrange(0,n)
-    #x=0
-    cadena=''
-    a, c, m = 45, 1, 2 ** 31
-    lcg = LCG(x,a,c,m)
-    for _ in range(n):
-        
-        bit = str(bin(next(lcg)))[2:]
-        if len(bit)<2:
-           cadena +=('0'+bit)
-        else:
-            cadena +=bit[-2:]
-    if len(cadena)>n:
-        
-        return cadena[n:]
-    else:
-        return cadena
+kbits = 1
+def LCG(m=2**31-1, a=1103515245, b=12345,n=10):
+    x = np.random.choice(m)
+    bits=""
+    for i in range(0, n):
+        x = (a*x + b) % m
+        stb = ''.join(format(ord(i), '08b') for i in str(x))
+        stb = stb[::-1]
+        for i in range(0, kbits):
+            bits+=stb[i]
+    return bits
