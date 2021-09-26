f205e173fa543f511a3eda550b97c6a59e6f2e24
Mar19845 <mar19845@uvg.edu.gt>
2021-09-25 23:43:25 -0600

    ejercicios 1,2,3,4 terminados

---
+++ Lab7/mcd.py
@@ -0,0 +1,59 @@
+import random
+def primo(num): #funcion para verificar Num primos
+    contador = 0
+    if num < 2:
+        return False
+    
+    for i in range (2, num):
+        if num % i == 0:
+            return False
+        
+    return True
+
+
+#Ejercicio 1
+#algoritmo de euclides para el mcd
+def mcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+#funcion para el inverso multiplicativo
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+
+#Ejercicio 3
+#algoritmo de euclides extendido para encontrar el inverso multiplicativo de dos numeros
+def inverso_multi(e, phi):
+    g, x, y = egcd(e, phi)
+    if g != 1:
+        return False
+    else:
+        return x % phi
+
+#Ejercicio 4
+#algoritmo de realizar el test de fermat
+#tomado de: https://gist.github.com/Ayrx/5884802
+def fermat_test(n, k=5):
+
+    # Implementation uses the Fermat Primality Test
+    
+    # If number is even, it's a composite number
+
+    if n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    for i in range(k):
+        a = random.randint(1, n-1)
+
+        if pow(a, n-1) % n != 1:
+            return False
+    return True
