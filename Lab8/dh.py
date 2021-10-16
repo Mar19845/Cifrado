6108dd8117d9aa2a60ba48b1a29c1eafd4cad651
Mar19845 <mar19845@uvg.edu.gt>
2021-10-15 20:10:21 -0600

    cambio

--- Lab8/dh.py
+++ Lab8/dh.py
@@ -8,9 +8,9 @@ def Diffie_Hellman(n=2):
     #private keys
     A_private_key = randint(2,p-1)
     B_private_key = randint(1,p-1)
-    A_public_key = p**A_private_key % q
-    B_public_key = p**B_private_key % q
+    A_public_key = pow(p,A_private_key,q) #p**A_private_key% q
+    B_public_key = pow(p,B_private_key,q) #p**B_private_key% q
     #calc a and b same keys
-    A = B_public_key**A_private_key % q
-    B = A_public_key**B_private_key % q
+    A = pow(B_public_key,A_private_key,q)#B_public_key**A_private_key % q
+    B = pow(A_public_key,B_private_key,q)#A_public_key**B_private_key % q
     return A, B
