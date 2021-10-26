1ec61fd64147bc707b615a84c154f70b2986f916
eduardorh1312 <53280540+eduardorh1312@users.noreply.github.com>
2021-10-26 17:42:43 -0600

    Create main.py

---
+++ Lab9/main.py
@@ -0,0 +1,21 @@
+# Universidad del Valle de Guatemala
+# Criptografia y cifrado de información
+# Laboratorio #9
+# Eduardo Ramírez Herrera 19946
+# Juan Manuel Marroquin 19845
+# Carlos Ráxtum 19721
+
+
+def __init__():
+
+    st = True
+    out = False
+
+    while(st):
+        print("\n***LABORATORIO 9***\n")
+        print("Cifrado RSA:\n 1. Generar Claves\n 2. Encriptar mensaje\n 3. Decriptar mensaje\n 4. Salir\n")
+        opt = input("Elija la funcion que desea ejecutar: ")
+        
+
+if __name__ == "__main__":
+    __init__()
